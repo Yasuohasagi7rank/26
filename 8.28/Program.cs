075d98b1b3c4961f241c9a0c367d6c7c9b097246
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:17:44 +0300

    Add files via upload

---
+++ 8.28/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _8._28
+{
+    class Program
+    {
+        static bool TrySimple(int Num)
+        {
+            bool a = true;
+            for (int i = 2; i <= Math.Sqrt(Num); i++)
+            {
+                if (Num % i == 0) a = false;
+            }
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            for (int a = 200; a <= 500; a++)
+            {
+                int c = 0;
+                for (int b = 2; b < a; b++)
+                {
+                    if (a % b == 0)
+                        if (TrySimple(b))
+                            c++;
+                }
+                if (c == 4) Console.Write(a + " ");
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
